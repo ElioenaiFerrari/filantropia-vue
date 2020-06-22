9aec1dd1a139b422b81450011692e4736aeaead4
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-06-22 18:13:50 -0300

    update

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,12 +1,22 @@
 <template>
   <v-layout column>
+    <v-alert
+      v-show="!isLoading && success != null"
+      :class="{ primary: success, error: !success }"
+      class="headline white--text font-weight-bold lighten-1"
+    >
+      <v-row class="px-2" align="center" justify="space-between">
+        {{ message }}
+        <v-btn outlined>
+          Ir para <span class="white--text">UNISALES</span>
+        </v-btn>
+      </v-row>
+    </v-alert>
     <v-card>
-      <v-card-title>
-        <span class="headline">{{
-          isLoading
-            ? 'Aguarde, seus dados estão sendo enviados.'
-            : 'Dados enviados com sucesso.'
-        }}</span>
+      <v-card-title v-show="isLoading">
+        <span class="headline">
+          Aguarde, seus dados estão sendo enviados.
+        </span>
 
         <v-progress-linear v-show="isLoading" class="mt-4" indeterminate />
       </v-card-title>
@@ -17,18 +27,26 @@
 <script>
 export default {
   async created() {
-    const { success } = await this.$http.post('/send', {});
+    const { data } = await this.$http.post('/send', {});
 
-    if (success) {
-      console.log(success);
-    } else {
-      console.log(success);
-    }
+    this.success = data.success;
+    this.isLoading = false;
+  },
+
+  computed: {
+    message() {
+      if (this.success) {
+        return 'Seus dados foram enviados com sucesso!';
+      } else {
+        return 'Ocorreu um erro no seu envio. Por favor, tente mais tarde.';
+      }
+    },
   },
 
   data() {
     return {
-      isLoading: false,
+      isLoading: true,
+      success: null,
     };
   },
 };
