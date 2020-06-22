df90485bc9ea308dedfc66e16eaa6c5c08c4f45f
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-06-22 11:40:50 -0300

    update

---
+++ src/components/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="model" persistent max-width="290">
+      <v-card class="pa-4">
+        <slot name="content" />
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error darken-1" text @click="onCancel">Cancelar</v-btn>
+          <v-btn color="primary darken-1" text @click="onCancel">Confirmar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ["model", "onClick", "disabled"],
+  methods: {
+    onCancel() {
+      this.$emit("onCancel");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Family.vue
+++ src/views/Family.vue
@@ -4,7 +4,7 @@
       <template v-slot:content>
         <v-col cols="12">
           <v-row>
-            <v-select
+            <!-- <v-select
               :prepend-inner-icon="
                 family.group.quantity > 1 ? 'mdi-account-group' : 'mdi-account'
               "
@@ -22,7 +22,7 @@
               "
               type="number"
               label="b) Total de mebros (Você + Grupo familiar)"
-            />
+            />-->
             <v-select
               prepend-inner-icon="mdi-home"
               v-model="family.housingType"
@@ -40,29 +40,64 @@
         </v-col>
       </template>
     </Chapter>
-    <Table
-      v-show="family.vehicle.has"
-      :items="family.vehicle.data"
-      :headers="tableHeaders.vehicle"
-      class="mb-4"
-    />
-    <Table :headers="tableHeaders.family" />
+
+    <v-col align="end" v-show="family.vehicle.has">
+      <Table :items="family.vehicle.data" :headers="tableHeaders.vehicle" class="mb-4" />
+      <v-btn @click="onShowDialogVehicle" color="primary darken-2" fab small dark>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-col>
+
+    <v-col align="end">
+      <Table :items="family.members" :headers="tableHeaders.family" />
+      <v-btn @click="onShowDialogMember" class="mt-4" color="primary darken-2" fab small dark>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-col>
 
     <v-row class="mt-4" align="center" justify="center">
       <v-btn to="/send" large width="300px" class="primary lighten-2 white--text">Concluir</v-btn>
     </v-row>
+
+    <Dialog
+      @onCancel="showDialogVehicle=false"
+      :disabled="false"
+      :onClick="onAddVehicle"
+      :model="showDialogVehicle"
+    >
+      <template v-slot:content>
+        <v-text-field v-model="dialogs.vehicle.model" label="Marca/Model" />
+        <v-text-field v-model="dialogs.vehicle.year" label="Ano" class="my-3" />
+        <v-select
+          v-model="dialogs.vehicle.utility"
+          label="Utilização"
+          :items="['Pessoal', 'Comercial']"
+        />
+      </template>
+    </Dialog>
+
+    <Dialog
+      @onCancel="showDialogMember=false"
+      :disabled="false"
+      :onClick="onAddMember"
+      :model="showDialogMember"
+    >
+      <template v-slot:content></template>
+    </Dialog>
   </v-layout>
 </template>
 
 <script>
 import Chapter from "@/components/templates/Chapter";
 import Table from "@/components/Table";
+import Dialog from "@/components/Dialog";
 import family from "@/database/family";
 
 export default {
   components: {
     Chapter,
-    Table
+    Table,
+    Dialog
   },
 
   computed: {
@@ -131,37 +166,76 @@ export default {
         ]
       },
       family: {
-        liveWith: "Sozinho",
+        // liveWith: "Sozinho",
         housingType: "Própria",
+        members: [],
         vehicle: {
           has: false,
-          data: []
-        },
-        group: {
-          quantity: 1,
-          data: []
+          data: [
+            {
+              model: "Ford",
+              year: "2020",
+              utility: "Pessoal"
+            }
+          ]
         }
-      }
+      },
+      dialogs: {
+        vehicle: {
+          model: "",
+          year: "",
+          utility: ""
+        },
+        members: {
+          name: "",
+          age: "",
+          relationship: "",
+          civilStatus: "",
+          scholarity: "",
+          profession: "",
+          income: 0
+        }
+      },
+      showDialogVehicle: false,
+      showDialogMember: false
     };
   },
 
-  watch: {
-    "family.group.quantity"(quantity) {
-      if (quantity > 1) {
-        this.family.liveWith = "Família";
-      } else {
-        this.family.liveWith = "Sozinho";
-      }
+  methods: {
+    onShowDialogVehicle() {
+      this.showDialogVehicle = true;
     },
 
-    "family.liveWith"(liveWith) {
-      if (liveWith == "Família") {
-        this.family.group.quantity = 2;
-      } else {
-        this.family.group.quantity = 1;
-      }
+    onShowDialogMember() {
+      this.showDialogMember = true;
+    },
+
+    onAddVehicle() {
+      this.showDialogVehicle = false;
+    },
+
+    onAddMember() {
+      this.showDialogMember = false;
     }
   }
+
+  // watch: {
+  //   "family.group.quantity"(quantity) {
+  //     if (quantity > 1) {
+  //       this.family.liveWith = "Família";
+  //     } else {
+  //       this.family.liveWith = "Sozinho";
+  //     }
+  //   },
+
+  //   "family.liveWith"(liveWith) {
+  //     if (liveWith == "Família") {
+  //       this.family.group.quantity = 2;
+  //     } else {
+  //       this.family.group.quantity = 1;
+  //     }
+  //   }
+  // }
 };
 </script>
 
